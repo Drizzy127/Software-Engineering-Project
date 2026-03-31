8daae9dfb821e55fcc37434aff4ac09afd646740
Shalini Daniel <131724369+shaliiid@users.noreply.github.com>
2026-03-31 10:26:58 -0400

    Delete SOFTWAREENGIN directory
    deleted the wrong files that were uploaded previously

--- SOFTWAREENGIN/src/main/Views/Controls/Launcher.java
+++
@@ -1,9 +0,0 @@
-package Controls;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(FILE_NAME_HERE.class, args);
-    }
-}
--- SOFTWAREENGIN/src/main/Views/Controls/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.softwareengin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.softwareengin to javafx.fxml;
-    exports org.example.softwareengin;
-}
